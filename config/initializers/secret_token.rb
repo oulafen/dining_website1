c9dea86c4bfb9aa0b2b03b46f3189b2307c649cd
xiaofen <oulafen@gmail.com>
2014-04-26 11:35:33 +0800

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DiningWebsite::Application.config.secret_key_base = 'a85651611fcad602617bf2890d2ce306aa8725b4105f926dea6f4a09078985651a412f92dd31be921a86b0aceb94f0059e7a6eb43bc6d42a2d3d28011a0bac81'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+DiningWebsite::Application.config.session_store :cookie_store, key: '_dining_website_session'
