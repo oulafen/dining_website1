a7e487cdb034cbb4181f43be87f2f90defd779b6
xiaofen <oulafen@gmail.com>
2014-04-27 15:51:24 +0800

    Complete the function create_login_session

--- app/controllers/merchant_controller.rb
+++ app/controllers/merchant_controller.rb
@@ -1,4 +1,33 @@
 class MerchantController < ApplicationController
-  def merchant_index
+
+  def login
+
   end
+
+  def logout
+    reset_session
+    redirect_to :login
+  end
+
+  def create_login_session
+    merchant = Merchant.find_by_user_name(params[:user][:user_name])
+    if merchant.nil? || !merchant.authenticate(params[:user][:password])
+      flash.now[:error] = '用户名或密码错误'
+      render :login
+    end
+    if merchant && merchant.authenticate(params[:user][:password])
+      session[:current_merchant] = merchant
+      redirect_to merchant.login_type=='user' ? :merchant_index : :admin_index
+    end
+  end
+
+  def merchant_index
+    if session[:current_merchant][:login_type] == 'user'
+      @restaurant_name = session[:current_merchant][:restaurant_name]
+    else
+      redirect_to :login
+    end
+  end
+
+
 end
