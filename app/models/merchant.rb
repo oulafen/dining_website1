d6e4db9019fb64f080704bbe83bd80fecf14f4f8
xiaofen <oulafen@gmail.com>
2014-04-27 14:23:11 +0800

    Add the validates for new_merchant

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,2 +1,8 @@
 class Merchant < ActiveRecord::Base
+  has_secure_password
+  validates :user_name , :presence => true, :uniqueness => {:case_sensitive => false}
+  validates :restaurant_name , :presence => true
+
+  attr_accessible :user_name, :restaurant_name, :password, :password_confirmation, :login_type
+  #has_many
 end
